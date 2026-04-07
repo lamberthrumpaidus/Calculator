84a55a54f3a59c17e2d448bab50365c83f052040
lamberthrumpaidus <lamberthpaulinusrumpaidus@gmail.com>
2026-04-07 16:48:41 +0700

    ubah isi File

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 10, b = 11;
+
+statict int Penjumblahan(int a, int b)
+{
+    return a + b;
+}
+
+statict int Pengurangan(int a, int b)
+{
+    return a - b;
+}
+
+statict int Perkalian(int a, int b) 
+{
+    return a*b;
+}
+
+statict int Pembagian(int a, int b) 
+{
+    return a/b;
+}
+
+console.WriteLine(a, " + " B, " = " +Penjumblahan(a,b))
+console.WriteLine(a, " - " B, " = " +Pengurangan(a,b))
+console.WriteLine(a, " - " B, " = " +Perkalian(a,b))
+console.WriteLine(a, " / " B, " = " +Pembagian(a,b))
